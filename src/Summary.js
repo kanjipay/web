0d7673f3b6cc052c01a6a123af8bffa5d99db8bf
James Neale <jamesn9226@gmail.com>
2022-02-21 21:39:31 +0000

    add button to restart flow

--- src/Summary.js
+++ src/Summary.js
@@ -1,10 +1,20 @@
 import React from "react";
 
-function Summary() {
+function Summary({ setPage }) {
+  const handleClick = () => {
+    setPage("details");
+  };
   return (
     <div className="App">
       <div className="text-2xl">Thank you!</div>
-      <div className="text-md">Payment Successful</div>
+      <div className="text-md">Payment Not Successful</div>
+      <button
+        className="btn btn-primary btn-block mb-4"
+        type="submit"
+        onClick={handleClick}
+      >
+        Make another payment?
+      </button>
     </div>
   );
 }
