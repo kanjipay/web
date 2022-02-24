2459b6ac5105d4a06de02bf37e567fad89d3c621
Matt Ffrench <matt.e.ffrench@gmail.com>
2022-02-24 10:20:40 +0000

    Export React components more concisely

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,9 +1,7 @@
 import { useParams } from 'react-router-dom';
 
-function Menu() {
+export default function Menu() {
   let { merchant_id } = useParams()
 
   return <h1>{merchant_id}</h1>
 }
-
-export default Menu
--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -1,5 +1,3 @@
-function NotFound() {
+export default function NotFound() {
   return <h1>Not found</h1>
 }
-
-export default NotFound
