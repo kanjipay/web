8adfb196c2723f40663a6c40209711d9bd051a40
Matt Ffrench <matt.e.ffrench@gmail.com>
2022-02-24 11:58:18 +0000

    Create desktop version of Home page and add function to compute window size

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,36 @@
 import { useEffect } from "react"
+import "./Home.css"
+import useWindowSize from "./utils/useWindowSize"
 
 export default function Home() {
+  const { width } = useWindowSize()
+
   useEffect(() => {
     document.title = "Home"
-  })
+  }, [])
 
   return (
-    <div>
-      <header>
-        <h1>Mercado</h1>
+    <div className="Home">
+
+      <header className="Home__header">
+        <div className="Home__headerContainer">
+          <h2 className="Home__headerLogo">Mercado</h2>
+        </div>
       </header>
+      <div className="Home__content">
+        <h1 className="Home__title">Bring your menu <span className="Home__titleHighlight">online</span></h1>
+        <p className="Home__salesCopy">Mercado is an online menu for food stalls</p>
+        <p className="Home__salesCopy">No set up fees. No subscription. All you need is a QR code.</p>
+        <img
+          src="/img/screenshots_desktop.png"
+          alt=""
+          className="Home__screenshotDesktop"
+        />
+      </div>
+
+      <footer className="Home__footer">
+
+      </footer>
     </div>
   )
 }
---
+++ src/utils/useWindowSize.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export default function useWindowSize() {
+  const isServerSideRendered = typeof window !== "undefined"
+
+  const [windowSize, setWindowSize] = useState({
+    width: isServerSideRendered ? 1200 : window.innerWidth,
+    height: isServerSideRendered ? 800 : window.innerHeight
+  })
+
+  function changeWindowSize() {
+    setWindowSize({ width: window.innerWidth, height: window.innerHeight })
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", changeWindowSize)
+
+    return (() => {
+      window.removeEventListener("resize", changeWindowSize)
+    })
+  }, [])
+
+  return windowSize
+}
