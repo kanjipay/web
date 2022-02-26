874ba9339f3b06d04986e0a5e5e1dd38b43e3530
James Neale <jamesn9226@gmail.com>
2022-02-26 12:50:45 +0000

    try to get CI to pass

--- src/scenes/JoinList/JoinList.jsx
+++ src/scenes/JoinList/JoinList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 // import './JoinList.css';
 function JoinList(props) {
   const { users, onSelectUser, onCloseOrderList } = props;
