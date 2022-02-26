20cb694254e81b439788d801c6606e29be2fb0b9
James Neale <jamesn9226@gmail.com>
2022-02-26 11:42:40 +0000

    adding skeleton order service

---
+++ src/Dashboard.jsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react';
+import * as FirestoreService from './services/firestore';
+
+import CreateList from './scenes/CreateList/CreateList';
+import JoinList from './scenes/JoinList/JoinList';
+import EditList from './scenes/EditList/EditList';
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+
+import useQueryString from './hooks/useQueryString';
+
+function MerchantDashboard() {
+    const [merchant, setMerchant] = useState("");
+    const [orderList, setOrderList] = useState("");
+    const [userId, setUserId] = useState("");
+    const [error, setError] = useState("");
+
+    const [orderListId, setOrderListId] = useQueryString('listId');
+
+    useEffect(() => {
+        FirestoreService.authenticateAnonymously()
+        .then(userCredential => {
+        setUserId(userCredential.user.uid);
+        //TODO make authentication user ID link to merchant ID / orderListID
+        //TODO makesure that the OrderList can be populated if the merchant is offline (this might work by default)
+        
+        if (orderListId) {
+            FirestoreService.getOrderList(orderListId)
+            .then(orderList => {
+                if (orderList.exists) {
+                    setError(null)
+                    setOrderList(orderList.data())
+                } else {
+                    setError('order-list-missing')
+                    setOrderListId();
+                }
+            })
+            .catch(() => setError('order-list-get-fail'));
+        }
+    })
+    .catch(() => setError('auth-failed'));
+    }, [orderListId, setOrderListId]);
+    
+
+    function onOrderListCreate(orderListId, userName) {
+        setOrderListId(orderListId);
+        setMerchant(userName);
+      }
+    
+    function onCloseOrderList() {
+        setOrderListId();
+        setOrderList();
+        setMerchant();
+    }
+
+
+    function onSelectUser(userName) {
+        setMerchant(userName);
+        FirestoreService.getOrderList(orderListId)
+          .then(updatedOrderList => setOrderList(updatedOrderList.data()))
+          .catch(() => setError('order-list-get-fail'));
+      }
+      
+  // render a scene based on the current state
+  if (orderList && merchant) {
+    return <EditList {...{ orderListId, merchant, onCloseOrderList, userId}}></EditList>;
+  } else if(orderList) {
+    return (
+      <div>
+        <ErrorMessage errorCode={error}></ErrorMessage>
+        <JoinList users={orderList.users} {...{orderListId, onSelectUser, onCloseOrderList, userId}}></JoinList>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <ErrorMessage errorCode={error}></ErrorMessage>
+
+      <CreateList onCreate={onOrderListCreate} userId={userId}></CreateList>
+    </div>
+  );
+
+}
+
+
+export default MerchantDashboard;
