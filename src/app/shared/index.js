dafd73ea7ae7ac0cff7f57f5401687e9d8ca10dc
Matt Ffrench <matt.e.ffrench@gmail.com>
2022-02-27 12:37:48 +0000

    refactor /src into new folder structure

--- src/app/shared/index.js
+++ src/app/shared/index.js
@@ -1,7 +1,7 @@
 // import 'dotenv/config'
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
