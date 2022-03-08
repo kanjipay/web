039396a00f88873e1339cd57a832779414b68517
Matt Ffrench <matt.e.ffrench@gmail.com>
2022-03-08 15:51:12 +0000

    Remove duplicate firebase app

--- src/app/shared/App.jsx
+++ src/app/shared/App.jsx
@@ -3,8 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../brand/HomePage";
 import NotFound from "./NotFoundPage";
 import MerchantDashboard from "../merchant/DummyDashboardPage";
-import CustomerForm from "../checkout/order/CustomerForm";
-import DummyOrderPage from "../merchant/DummyOrderPage";
 import { BrowserRouter } from "react-router-dom";
 import Menu from "../checkout/menu/Menu";
 import { v4 as uuid } from 'uuid';
@@ -29,11 +27,6 @@ export default function App() {
           path="/merchant/test/dashboard/385as2als921hsa"
           element={<MerchantDashboard />}
         />
-        <Route
-          path="/checkout/test/385as2als921hsa"
-          element={<DummyOrderPage />}
-        />
-        <Route path="/checkout/test/109sjkba2al21s" element={<CustomerForm />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
