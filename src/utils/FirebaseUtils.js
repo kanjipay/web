6a0f7d96d420ac08f4457f6e78450800808d6288
Matthew Ffrench <matt.e.ffrench@gmail.com>
2022-03-15 14:55:43 +0000

    Make app check work locally

--- src/utils/FirebaseUtils.js
+++ src/utils/FirebaseUtils.js
@@ -27,15 +27,20 @@ const functions = getFunctions(firebaseApp);
 
 const environment = process.env.REACT_APP_ENV_NAME;
 
-if (environment === Environment.PROD) {
-  initializeAppCheck(firebaseApp, {
-    provider: new ReCaptchaV3Provider(
-      process.env.REACT_APP_FIREBASE_RECAPTCHA_PUBLIC_ID
-    ),
-    isTokenAutoRefreshEnabled: true,
-  });
-} else if (environment === Environment.DEV) {
-  connectFunctionsEmulator(functions, "localhost", 5001);
+console.log("environment: ", environment)
+
+if (environment === Environment.DEV) {
+  // window.FIREBASE_APPCHECK_DEBUG_TOKEN = true
+  connectFunctionsEmulator(functions, "localhost", 5000);
 }
+
+initializeAppCheck(firebaseApp, {
+  provider: new ReCaptchaV3Provider(
+    process.env.REACT_APP_FIREBASE_RECAPTCHA_PUBLIC_ID
+  ),
+  isTokenAutoRefreshEnabled: true,
+});
+
+
 
 export { firebaseApp, db, storage, functions };
