8e294e3a1b735067369cd18c1dcf9ecea9691fb1
James Neale <42097394+james9226@users.noreply.github.com>
2022-03-27 19:33:36 +0100

    Add logging to TL Webhook

--- functions/src/webhook/truelayer/verify.ts
+++ functions/src/webhook/truelayer/verify.ts
@@ -1,13 +1,23 @@
 import * as tlSigning from "truelayer-signing";
 import { truelayerUrlName } from "../../utils/truelayerClient";
 import axios from "axios";
+import * as functions from "firebase-functions";
 
 let cacheDate: Date | null;
 let cachedJwks: string | null;
 
-function parseRawHeaders(arr: string[]) {
+function parseRawHeaders(arr: string[], correlationId: string[]) {
+  functions.logger.log('Parsing headers for Truelayer Webhook Request', {      
+    correlationId: correlationId,  
+  });
+
+
   const len = arr.length;
   if (len % 2 !== 0) {
+    functions.logger.error('Header has odd number of elements', {
+      correlationId: correlationId,  
+      header: arr,
+    })
     throw new Error("Headers array cannot have an odd number of elements");
   }
 
@@ -29,20 +39,30 @@ function parseRawHeaders(arr: string[]) {
   return headers;
 }
 
-export const verify = async (req) => {
-  const headers = parseRawHeaders(req.rawHeaders);
-  console.log("Headers: ", JSON.stringify(headers));
+export const verify = async (req, correlationId) => {
+  const headers = parseRawHeaders(req.rawHeaders, correlationId);
+  functions.logger.log('Truelayer Webhook Request Headers', {
+    correlationId:correlationId,
+    headers: headers
+  });
   const tlSignature = headers["Tl-Signature"];
 
-  console.log("tlSignature: ", tlSignature);
 
   if (!tlSignature) {
+    functions.logger.error('Truelayer Signature Missing', {
+      correlationId:correlationId,
+    })
     return false;
   }
 
   const jku = tlSigning.extractJku(tlSignature);
 
   if (jku !== `https://webhooks.${truelayerUrlName()}.com/.well-known/jwks`) {
+    functions.logger.error('JKU does not match expected version', {
+      correlationId:correlationId,
+      headerJKU:jku,
+      expectedJKU:`https://webhooks.${truelayerUrlName()}.com/.well-known/jwks`,
+    })
     return false;
   }
 
@@ -51,18 +71,20 @@ export const verify = async (req) => {
   let jwks: string;
 
   if (cachedJwks && cacheDate && cacheDate > oneDayAgo) {
-    console.log("cached jwks found");
+
+    functions.logger.log('Cached jwks found', {
+      correlationId:correlationId,
+    });
     jwks = cachedJwks;
   } else {
-    console.log("no cached jwks, retrieving from url");
+    functions.logger.log('No cached jwks, retrieving from url', {
+      correlationId:correlationId,
+    });
     const res = await axios.get(jku);
-    console.log("jwks: ", JSON.stringify(res.data));
     jwks = JSON.stringify(res.data);
   }
 
   const body = JSON.stringify(req.body);
-
-  console.log("body: ", body);
 
   try {
     tlSigning.verify({
@@ -74,10 +96,16 @@ export const verify = async (req) => {
       headers,
     });
 
+
+    functions.logger.log('Truelayer verification sucessful', {
+      correlationId:correlationId,
+    })
     return true;
   } catch (err) {
-    console.log(err);
-    console.log(err.message);
+    functions.logger.error('Failed to verify Truelayer signature', {
+      correlationId:correlationId,
+      error:err,
+    })
     return false;
   }
 };
