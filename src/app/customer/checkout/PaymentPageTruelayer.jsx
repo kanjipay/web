4462c215456fb8f4320d3553e270d3c43e042dc4
James Neale <42097394+james9226@users.noreply.github.com>
2022-03-28 23:11:22 +0100

    Make prod app use TL Sandbox

--- src/app/customer/checkout/PaymentPageTruelayer.jsx
+++ src/app/customer/checkout/PaymentPageTruelayer.jsx
@@ -21,18 +21,19 @@ export default function PaymentPageTruelayer() {
         const { resourceToken, paymentId } = res.data.truelayer;
 
         // This should likely be in the backend?
-        const appName =
-          process.env.REACT_APP_ENV_NAME === "PROD"
-            ? "truelayer"
-            : "truelayer-sandbox";
+        // Hardcode to truelayer sandbox until we gain full prod access
+        // const appName = 
+          // process.env.REACT_APP_ENV_NAME === "PROD"
+          //   ? "truelayer"
+          //   : "truelayer-sandbox";
         const redirectUrl = new URL(window.location.href);
         redirectUrl.pathname = "/tl-redirect";
-        const truelayerUrl = `https://payment.${appName}.com/payments#payment_id=${paymentId}&resource_token=${resourceToken}&return_uri=${redirectUrl.href}`;
+        console.log(truelayerUrl);
+        const truelayerUrl = `https://payment.truelayer-sandbox.com/payments#payment_id=${paymentId}&resource_token=${resourceToken}&return_uri=${redirectUrl.href}`;
 
         AnalyticsManager.main.logEvent(AnalyticsEvent.CREATE_PAYMENT_ATTEMPT, {
           paymentAttemptId,
         });
-
         window.location.href = truelayerUrl;
       })
       .catch((err) => {
