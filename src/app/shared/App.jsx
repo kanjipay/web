937d595817666cbbe7bac713b6f2b7e341969623
Matthew Ffrench <matt.e.ffrench@gmail.com>
2022-04-05 14:06:20 +0100

    Add sales and demo pages

--- src/app/shared/App.jsx
+++ src/app/shared/App.jsx
@@ -1,26 +1,24 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../brand/HomePage";
-import NotFound from "./NotFoundPage";
 import { BrowserRouter } from "react-router-dom";
 import Menu from "../customer/menu/Menu";
 import MerchantApp from "../merchant/MerchantApp";
 import RedirectPageTruelayer from "../customer/checkout/RedirectPageTruelayer";
 import Checkout from "../customer/checkout/Checkout";
+import { Brand } from "../brand/Brand";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-
         <Route path="/menu/:merchantId/*" element={<Menu />} />
         <Route path="/checkout/:orderId/*" element={<Checkout />} />
 
         <Route path="/tl-redirect" element={<RedirectPageTruelayer />} />
 
         <Route path="/merchant/*" element={<MerchantApp />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<Brand />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/utils/variables.js
@@ -0,0 +1,1 @@
+export const transactionFee = 0.009
