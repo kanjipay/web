27223b25b77343fc74aa099d49e5cdf1cf8b0f05
James Neale <42097394+james9226@users.noreply.github.com>
2022-04-10 11:09:33 +0100

    Lint and finish logging refactor

--- functions/src/api/controllers/AnalyticsController.ts
+++ functions/src/api/controllers/AnalyticsController.ts
@@ -1,21 +1,17 @@
-
 import BaseController from "./BaseController";
 import { db } from "../../utils/admin";
 import Collection from "../../enums/Collection";
 
 export default class AnalyticsController extends BaseController {
-    log = async (req, res, next) => {
-            const data = req.body;
-            
-            await db()
-            .collection(Collection.WEB_AUDIT_LOG)
-            .doc(data.deviceId)
-            .collection('events')
-            .add(
-                data
-            );
-            
-        
-        return res.end();
-    };
-};
+  log = async (req, res, next) => {
+    const data = req.body;
+
+    await db()
+      .collection(Collection.WEB_AUDIT_LOG)
+      .doc(data.deviceId)
+      .collection("events")
+      .add(data);
+
+    return res.end();
+  };
+}
--- functions/src/utils/plaidClient.ts
+++ functions/src/utils/plaidClient.ts
@@ -6,7 +6,6 @@ import {
   PlaidEnvironments,
   Products,
 } from "plaid";
-import * as functions from "firebase-functions";
 
 let plaidInstance: PlaidApi | null = null;
 
@@ -76,7 +75,7 @@ export async function createLinkToken(
   payment_id: string,
   client_user_id: string,
   isLocalEnvironment: boolean,
-  correlationId: string
+  loggingClient
 ) {
   let plaidRedirectUri = "";
 
@@ -88,9 +87,7 @@ export async function createLinkToken(
     plaidRedirectUri = "https://mercadopay-dev.web.app/checkout/payment";
   }
 
-  functions.logger.log("Make Link Token Configuration Settings", {
-    correlationId: correlationId,
-    isLocalEnvironment: isLocalEnvironment,
+  loggingClient.log("Plaid Link Token Configuration Set", {
     plaidRedirectUri: plaidRedirectUri,
     payment_id: payment_id,
     client_user_id: client_user_id,
@@ -102,31 +99,31 @@ export async function createLinkToken(
   //     ? "https://mercadopay.co/checkout/payment"
   //     : "https://mercadopay-dev.web.app/checkout/payment";
 
-  try {const res = await plaidClient().linkTokenCreate({
-    user: {
-      client_user_id,
-    },
-    client_name: "Mercado",
-    products: [Products.PaymentInitiation],
-    country_codes: [CountryCode.Gb],
-    language: "en",
-    webhook: `${process.env.WEBHOOK_URL}/plaid`,
-    redirect_uri: plaidRedirectUri,
-    payment_initiation: {
-      payment_id,
-    },
-  })
+  try {
+    const res = await plaidClient().linkTokenCreate({
+      user: {
+        client_user_id,
+      },
+      client_name: "Mercado",
+      products: [Products.PaymentInitiation],
+      country_codes: [CountryCode.Gb],
+      language: "en",
+      webhook: `${process.env.WEBHOOK_URL}/plaid`,
+      redirect_uri: plaidRedirectUri,
+      payment_initiation: {
+        payment_id,
+      },
+    });
 
-  return res.data;}
-  catch (err) {
-    functions.logger.log("Make Link Token Error", {
-      correlationId: correlationId,
+    return res.data;
+  } catch (err) {
+    loggingClient.error("Make Link Token Error", {
       isLocalEnvironment: isLocalEnvironment,
       plaidRedirectUri: plaidRedirectUri,
       payment_id: payment_id,
       client_user_id: client_user_id,
-      webhook: process.env.WEBHOOK_URL, 
-      error:err
-    });  
-  };
-};
+      webhook: process.env.WEBHOOK_URL,
+      error: err,
+    });
+  }
+}
