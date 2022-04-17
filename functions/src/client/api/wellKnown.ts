3629078e652db2e50e852ac35011ffb9629e791e
oli5679 <olicairns93@gmail.com>
2022-04-17 20:30:04 +0100

    wip

--- functions/src/client/api/wellKnown.ts
+++ functions/src/client/api/wellKnown.ts
@@ -1,6 +1,16 @@
+import * as jose from "node-jose";
+import * as ms from "ms";
 import { Router } from "express";
 
 const routes = Router();
+
+let KEYSTORE = jose.JWK.createKeyStore();
+let ROTATE_TIME = Date.now();
+
+async function rotateKey() {
+  KEYSTORE = jose.JWK.createKeyStore();
+  KEYSTORE.generate("RSA", 2048, { alg: "RS256", use: "sig" });
+}
 
 routes.get("/config", async (req, res, next) => {
   res.status(200).json({
@@ -9,9 +19,13 @@ routes.get("/config", async (req, res, next) => {
 })
 
 routes.get("/jwks", async (req, res, next) => {
-  res.status(200).json({
-    keys: []
-  })
+  if (Date.now() > ROTATE_TIME){
+    rotateKey();
+    ROTATE_TIME = Date.now() + ms('1d');
+  }
+  res.status(200).json(KEYSTORE.toJSON())
 })
+
+rotateKey();
 
 export default routes
