cd5c37e482ffcc578151b4bcb483a86904c69245
oli5679 <olicairns93@gmail.com>
2022-05-02 13:10:39 +0100

    add webhook

---
+++ functions/src/internal/webhooks/crezco/handleCrezcoPaymentUpdate.ts
@@ -0,0 +1,40 @@
+
+
+import LoggingController from "../../../shared/utils/loggingClient";
+import PaymentAttemptStatus from "../../../shared/enums/PaymentAttemptStatus";
+
+
+import { updatePaymentAttemptIfNeeded } from "../../api/updatePaymentAttempt";
+const crezcoPaymentStatuses = {
+    "PaymentCompleted": PaymentAttemptStatus.SUCCESSFUL,
+    "PaymentPending": PaymentAttemptStatus.PENDING,
+    "PaymentFailed": PaymentAttemptStatus.FAILED
+  }
+
+
+export const handleCrezcoPaymentUpdate = async (req, res, next) => {
+  try {
+    const loggingClient = new LoggingController("Crezco Webhook");
+
+    const [{eventType, id, metadata}] = req.body // Todo create some verification middleware
+    loggingClient.log("Request received", { req })
+    loggingClient.log("Payload received", { eventType, id, metadata })
+    
+    console.log(Object.keys(crezcoPaymentStatuses))
+    console.log(eventType in crezcoPaymentStatuses)
+
+    if (!eventType || !(eventType in crezcoPaymentStatuses)) {
+      loggingClient.log("Non-payment event type received", { eventType })
+      return res.sendStatus(200)
+    }
+    const paymentAttemptStatus = crezcoPaymentStatuses[eventType]
+    const { paymentDemandId } = metadata
+    await updatePaymentAttemptIfNeeded(id, paymentDemandId, paymentAttemptStatus)
+    loggingClient.log("updating payement received", { id, paymentDemandId, paymentAttemptStatus })
+    res.sendStatus(200)
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
+  }
+  
+}
--- functions/src/internal/webhooks/webhooksApp.ts
+++ functions/src/internal/webhooks/webhooksApp.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import * as cors from "cors";
 import { checkMoneyhubIp } from "./moneyhub/checkMoneyhubIp";
 import { handleMoneyhubPaymentUpdate } from "./moneyhub/handleMoneyhubPaymentUpdate";
+import { handleCrezcoPaymentUpdate } from "./crezco/handleCrezcoPaymentUpdate";
 import { verifyMoneyhub } from "./moneyhub/verifyMoneyhub";
 
 const webhooksApp = express();
@@ -9,12 +10,18 @@ const webhooksApp = express();
 const corsInstance = cors({ origin: "*" });
 webhooksApp.use(corsInstance);
 webhooksApp.options("*", corsInstance);
+webhooksApp.use(express.json());
 
 webhooksApp.post(
   "/moneyhub", 
   checkMoneyhubIp, 
   verifyMoneyhub,
   handleMoneyhubPaymentUpdate
+);
+
+webhooksApp.post(
+  "/crezco", 
+  handleCrezcoPaymentUpdate
 );
 
 export default webhooksApp;
