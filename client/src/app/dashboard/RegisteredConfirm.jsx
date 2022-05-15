d9a72ebb5245dafb101fde78072316a6e1fe155f
oli5679 <olicairns93@gmail.com>
2022-05-15 14:41:20 +0100

    lint

--- client/src/app/dashboard/RegisteredConfirm.jsx
+++ client/src/app/dashboard/RegisteredConfirm.jsx
@@ -1,31 +1,39 @@
-import {  useState } from "react";
+import { useState } from "react";
 import Tick from "../../assets/icons/Tick";
 import { Colors } from "../../components/CircleButton";
 import IconPage from "../../components/IconPage";
 import LoadingPage from "../../components/LoadingPage";
 import { NetworkManager, ApiName } from "../../utils/NetworkManager";
 
-
-
-
 export default function RegisteredConfirm() {
   const [registered, setRegistered] = useState(false);
-  const queryParams = new URLSearchParams(window.location.search)
+  const queryParams = new URLSearchParams(window.location.search);
   const merchantId = queryParams.get("merchant-id");
   const crezcoUserId = queryParams.get("user-id");
-  const paymentRegisterBody = {merchantId,crezcoUserId}
-  
-  console.log('registered')
-  if (registered){
-    return <IconPage 
-    Icon={Tick}
-    iconBackgroundColor={Colors.BLACK}
-    iconForegroundColor={Colors.WHITE}
-    title="Thank you for registering!"
-    body="We have registered you as an organiser"
-  />
+  const paymentRegisterBody = { merchantId, crezcoUserId };
+
+  console.log("registered");
+  if (registered) {
+    return (
+      <IconPage
+        Icon={Tick}
+        iconBackgroundColor={Colors.BLACK}
+        iconForegroundColor={Colors.WHITE}
+        title="Thank you for registering!"
+        body="We have registered you as an organiser"
+      />
+    );
   } else {
-    NetworkManager.post(ApiName.INTERNAL, '/payees/update',  paymentRegisterBody).then(setRegistered(true))
-    return <LoadingPage iconBackgroundColor={Colors.BLACK} iconForegroundColor={Colors.WHITE}/>
+    NetworkManager.post(
+      ApiName.INTERNAL,
+      "/payees/update",
+      paymentRegisterBody
+    ).then(setRegistered(true));
+    return (
+      <LoadingPage
+        iconBackgroundColor={Colors.BLACK}
+        iconForegroundColor={Colors.WHITE}
+      />
+    );
   }
 }
--- functions/src/onlineMenu/api/controllers/MerchantsController.ts
+++ functions/src/onlineMenu/api/controllers/MerchantsController.ts
@@ -3,42 +3,51 @@ import { db } from "../../../shared/utils/admin";
 import Collection from "../../../shared/enums/Collection";
 import LoggingController from "../../../shared/utils/loggingClient";
 import { firestore } from "firebase-admin";
-import {v4} from 'uuid';
+import { v4 } from "uuid";
 
 export default class MerchantsController extends BaseController {
   create = async (req, res, next) => {
-        const loggingClient = new LoggingController("Merchant Controller");
-        loggingClient.log(
-            "Merchant creation started",
-            {
-            environment: process.env.ENVIRONMENT,
-            clientURL: process.env.CLIENT_URL,
-            },
-            req.body
-        );
-        
-        const { accountNumber, address, companyName, displayName, sortCode } = req.body;
-        const payeeId = v4();
-        await db()
-        .collection(Collection.PAYEE)
-        .doc(payeeId)
-        .set({
-            accountNumber, address, companyName, sortCode,
-            createdAt: firestore.FieldValue.serverTimestamp(),
-            status:  "PENDING",
-        });
-        const merchantId = v4();
-        await db()
-        .collection(Collection.MERCHANT)
-        .doc(merchantId)
-        .set({
-            accountNumber, companyName, displayName, sortCode, payeeId,
-            createdAt: firestore.FieldValue.serverTimestamp(),
-            status: "PENDING",
-
-        });
-        loggingClient.log("Merchant document creation complete", {}, { merchantId, payeeId });
-        loggingClient.log("Merchant subcollection creation complete");
-        return res.status(200).json({ merchantId });
-        }
+    try {
+      const loggingClient = new LoggingController("Merchant Controller");
+      loggingClient.log(
+        "Merchant creation started",
+        {
+          environment: process.env.ENVIRONMENT,
+          clientURL: process.env.CLIENT_URL,
+        },
+        req.body
+      );
+      const { accountNumber, address, companyName, displayName, sortCode } =
+        req.body;
+      const payeeId = v4();
+      await db().collection(Collection.PAYEE).doc(payeeId).set({
+        accountNumber,
+        address,
+        companyName,
+        sortCode,
+        createdAt: firestore.FieldValue.serverTimestamp(),
+        status: "PENDING",
+      });
+      const merchantId = v4();
+      await db().collection(Collection.MERCHANT).doc(merchantId).set({
+        accountNumber,
+        companyName,
+        displayName,
+        sortCode,
+        payeeId,
+        createdAt: firestore.FieldValue.serverTimestamp(),
+        status: "PENDING",
+      });
+      loggingClient.log(
+        "Merchant document creation complete",
+        {},
+        { merchantId, payeeId }
+      );
+      loggingClient.log("Merchant subcollection creation complete");
+      return res.status(200).json({ merchantId });
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+  };
 }
