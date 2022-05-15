13da78b0f1e64f271f92dfced5c36d7fa15e274a
oli5679 <olicairns93@gmail.com>
2022-05-15 14:27:37 +0100

    i think it works

--- client/src/app/dashboard/RegisteredConfirm.jsx
+++ client/src/app/dashboard/RegisteredConfirm.jsx
@@ -3,8 +3,6 @@ import Tick from "../../assets/icons/Tick";
 import { Colors } from "../../components/CircleButton";
 import IconPage from "../../components/IconPage";
 import LoadingPage from "../../components/LoadingPage";
-import {updateDoc } from "firebase/firestore";
-import Collection  from "../../enums/Collection";
 import { NetworkManager, ApiName } from "../../utils/NetworkManager";
 
 
@@ -14,21 +12,20 @@ export default function RegisteredConfirm() {
   const [registered, setRegistered] = useState(false);
   const queryParams = new URLSearchParams(window.location.search)
   const merchantId = queryParams.get("merchant-id");
-  const crezcoId = queryParams.get("user-id");
-  const paymentRegisterBody = {merchantId:crezcoId}
-  NetworkManager.post(ApiName.INTERNAL, '/merchants/create',  paymentRegisterBody)
+  const crezcoUserId = queryParams.get("user-id");
+  const paymentRegisterBody = {merchantId,crezcoUserId}
+  
   console.log('registered')
   if (registered){
     return <IconPage 
     Icon={Tick}
-    iconBackgroundColor={Colors.PRIMARY_LIGHT}
-    iconForegroundColor={Colors.PRIMARY}
-    title="Registered with Crezco"
-    body="We have registered you with our payment processing partner, Crezco"
+    iconBackgroundColor={Colors.BLACK}
+    iconForegroundColor={Colors.WHITE}
+    title="Thank you for registering!"
+    body="We have registered you as an organiser"
   />
   } else {
-    /*SavePayeeDetails(merchantId, crezcoId)*/
-    setRegistered(true)
-    return <LoadingPage/>
+    NetworkManager.post(ApiName.INTERNAL, '/payees/update',  paymentRegisterBody).then(setRegistered(true))
+    return <LoadingPage iconBackgroundColor={Colors.BLACK} iconForegroundColor={Colors.WHITE}/>
   }
 }
