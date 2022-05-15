e2cb9b057437963f29c3609cbeb86eba93e4ebd8
oli5679 <olicairns93@gmail.com>
2022-05-15 14:31:37 +0100

    reorganise

--- client/src/utils/helpers/validation.js
+++ client/src/utils/helpers/validation.js
@@ -1,3 +1,10 @@
 export function validateEmail(value) {
   return /\S+@\S+\.\S+/.test(value);
 }
+export function validateBankAccountNumber(bankAccountNumber){
+  return /^(\d){7,8}$/.test(bankAccountNumber);
+}
+
+export function validateSortCode(sortCode){
+  return /^(\d){6}$/.test(sortCode);
+}
