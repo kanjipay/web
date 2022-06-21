c2dae393a0021a8207ddd9f0a56c4c555c57c4be
Matthew Ffrench <matt.e.ffrench@gmail.com>
2022-06-21 10:44:48 +0100

    Make hotjar integration react to environment

--- client/src/utils/AnalyticsManager.js
+++ client/src/utils/AnalyticsManager.js
@@ -1,6 +1,7 @@
 import amplitude from "amplitude-js";
 import { IdentityManager } from "./IdentityManager";
 import { hotjar } from 'react-hotjar';
+import Environment from "../enums/Environment";
 
 export class AnalyticsEvent {
   static VIEW_PAGE = "ViewPage";
@@ -39,10 +40,12 @@ export class AnalyticsManager {
     const pseudoUserId = IdentityManager.main.getPseudoUserId()
     
     analytics.init(process.env.REACT_APP_AMPLITUDE_API_KEY, pseudoUserId, options);
-    
-    hotjar.initialize(3030022, 6)
-    hotjar.identify(pseudoUserId)
 
+    if ([Environment.PROD, Environment.STAGING].includes(process.env.REACT_APP_ENV_NAME)) {
+      hotjar.initialize(process.env.REACT_APP_HOTJAR_ID, 6)
+      hotjar.identify(pseudoUserId)
+    }
+    
     // /*
     // I don't believe we want to use the default device id, it's just a uuid in cookies,
     // so will be removed when user clears cookies, or null in private browsing mode
