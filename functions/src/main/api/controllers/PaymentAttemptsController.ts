1945c73e29da26d7664ab65a668149db009b9a74
oli5679 <olicairns93@gmail.com>
2022-06-30 17:45:48 +0100

    rm log

--- functions/src/main/api/controllers/PaymentAttemptsController.ts
+++ functions/src/main/api/controllers/PaymentAttemptsController.ts
@@ -28,7 +28,6 @@ const crezcoPaymentStatuses = {
 export class PaymentAttemptsController extends BaseController {
   createStripe = async (req, res, next) => {
     try {
-      console.log('here0')
       const logger = new LoggingController("Create payment attempt with stripe")
       const { orderId, deviceId } = req.body;
 
@@ -134,7 +133,6 @@ export class PaymentAttemptsController extends BaseController {
       const { total, currency, merchantId } = order
 
       logger.log("Got order", { order })
-      console.log('here')
       const { merchant, merchantError } = await fetchDocument(Collection.MERCHANT, merchantId)
 
       logger.log(`Retrieving merchant with id ${merchantId}`)
@@ -161,7 +159,6 @@ export class PaymentAttemptsController extends BaseController {
         total,
         currency
       })
-      console.log('here2')
       const { paymentDemandId, payDemandError } = await createPaymentDemand(
         crezcoUserId,
         paymentAttemptId,
@@ -177,7 +174,6 @@ export class PaymentAttemptsController extends BaseController {
       }
 
       logger.log("Created crezco paymentDemandId", {}, { paymentDemandId })
-      console.log('here3')
       const { redirectUrl, paymentError } = await createPayment(
         crezcoUserId,
         paymentDemandId,
@@ -185,7 +181,6 @@ export class PaymentAttemptsController extends BaseController {
         crezcoBankCode,
         countryCode
       )
-      console.log('here4')
       if (paymentError) {
         next(paymentError)
         return
@@ -214,7 +209,6 @@ export class PaymentAttemptsController extends BaseController {
         .catch(new ErrorHandler(HttpStatusCode.INTERNAL_SERVER_ERROR, next).handle)
 
       logger.log("Payment attempt doc added", { paymentAttemptData });
-      console.log('here5')
       return res.status(200).json({ redirectUrl });
     } catch (err) {
       console.log(err.data?.errors)
