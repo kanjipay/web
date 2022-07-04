da03e1a35214fbf3153b0b03a8637c6bf10b5c79
oli5679 <olicairns93@gmail.com>
2022-07-04 17:07:28 +0100

    update

--- functions/test/api/banks/getBanksData.test.ts
+++ functions/test/api/banks/getBanksData.test.ts
@@ -5,7 +5,15 @@ describe("Get Banks Data", () => {
     api
       .get('/banks/GB')
       .end(function(err, res) {
+        console.log(res.body);
         expect(res).to.have.status(200);
+        expect(res.body.length).to.be.above(0);
+        const missingBankCode = res.body.filter(item => item.bankCode == undefined).length
+        expect(missingBankCode).to.be.equal(0);
+        const missingBankName = res.body.filter(item => item.bankName == undefined).length
+        expect(missingBankName).to.be.equal(0);
+        const missingLogoUrl= res.body.filter(item => item.logoUrl == undefined).length
+        expect(missingLogoUrl).to.be.equal(0);  
         done();                              
       });
    
