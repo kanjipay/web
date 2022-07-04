5e543485d3cd49adb55347b836688e966330268c
oli5679 <olicairns93@gmail.com>
2022-07-04 18:59:04 +0100

    stripe payment attempt

--- functions/test/api/payment-attempt/payment-attempt.test.ts
+++ functions/test/api/payment-attempt/payment-attempt.test.ts
@@ -1,32 +1,43 @@
 
 import "mocha"
 import { api, expect } from "../../utils/server";
-import { createMerchant, createTicketOrder } from "../../utils/generateTestData";
+import {  createTicketOrder } from "../../utils/generateTestData";
 
 import { db } from "../../utils/admin";
 import Collection from "../../../src/shared/enums/Collection";
 
 describe("Payment attempts", () => {
-  const orderId = 'test-get-payment-attempt-order';
-  const merchantId = 'test-merchant';
-  const paymentAttemptData = {orderId,
-                            crezcoBankCode:'mock-payments-gb-redirect', 
-                            countryCode :'GB', 
+  const orderIdCrezco = 'test-get-payment-attempt-order-crez';
+  const orderIdStripe = 'test-get-payment-attempt-order-str';
+  const merchantId = 'trinity';
+  const paymentAttemptStripeData = {orderId:orderIdCrezco,
                             deviceId:'123'};
+  const paymentAttemptCrezcoData = {orderId:orderIdStripe,
+    crezcoBankCode:'mock-payments-gb-redirect', 
+    countryCode :'GB', 
+    deviceId:'123'};
   before(async () => {
-     createTicketOrder(orderId, 100, merchantId);
-     createMerchant(merchantId);
+     await createTicketOrder(orderIdCrezco, 100, merchantId);
+     await createTicketOrder(orderIdStripe, 100, merchantId);
   });  
   it("Create crezco payment",  (done) => {
         api.post('/payment-attempts/crezco')
-           .send(paymentAttemptData)
+           .send(paymentAttemptCrezcoData)
            .end(function(err, res) {
             expect(res).to.have.status(200);
             done();
         });
     })
+    it("Create stripe payment",  (done) => {
+      api.post('/payment-attempts/stripe')
+         .send(paymentAttemptStripeData)
+         .end(function(err, res) {
+          expect(res).to.have.status(200);
+          done();
+      });
+  })
   after(async () => {
-    await db.collection(Collection.ORDER).doc(orderId).delete();
-    await db.collection(Collection.MERCHANT).doc(merchantId).delete();
+    await db.collection(Collection.ORDER).doc(orderIdCrezco).delete();
+    await db.collection(Collection.ORDER).doc(orderIdStripe).delete();
   });
 });
