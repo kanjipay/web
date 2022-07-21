1ac684672fd55ca9fa70bc3676be6b245fda0bec
oli5679 <olicairns93@gmail.com>
2022-07-21 13:42:05 +0100

    wip

---
+++ client/src/app/shared/terms/OrganiserTerms.jsx
@@ -0,0 +1,22 @@
+import { Helmet } from "react-helmet-async"
+import "./OrganiserTerms.css"
+
+
+export default function OrganiserTerms() {
+  return (
+    <div
+    >
+      <Helmet>
+        <title>Organiser Terms</title>
+      </Helmet>
+      <h1>
+        Organiser Terms
+      </h1>
+      <p></p>
+<p>Last Updated 28 June 2022</p>
+<h2>Definitions</h2>
+<p><strong>TERMS</strong> this agreement, defining an Organiser’s terms of service. </p>
+
+    </div>
+  )
+}
