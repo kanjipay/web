b3048a75e1039b41d8dc75936d9e680ea5e6d61f
oli5679 <olicairns93@gmail.com>
2022-09-04 16:26:02 +0100

    wip

--- functions/src/shared/utils/googleWallet.ts
+++ functions/src/shared/utils/googleWallet.ts
@@ -1,5 +1,13 @@
 import {GoogleAuth} from 'google-auth-library';
 import * as jwt from "jsonwebtoken"
+import * as base64 from "base-64"
+
+const CREDENTIALS = JSON.parse(base64.decode(process.env.SERVICE_ACCOUNT))
+const GOOGLE_WALLET_ISSUER_ID = process.env.GOOGLE_WALLET_ISSUER_ID
+const HTTP_CLIENT = new GoogleAuth({
+  credentials:CREDENTIALS,
+  scopes: 'https://www.googleapis.com/auth/wallet_object.issuer'
+});  
 
 export type GoogleTicketDetail = {
   ticketId: string;
@@ -9,13 +17,44 @@ export type GoogleTicketDetail = {
   ticketHolderName: string;
 }
 
-async function  createGooglePassTicket(classId: string, issuerId: string, ticketDetail: GoogleTicketDetail, httpClient: GoogleAuth){
+export async function createGooglePassEventClass(eventData){
+  const {eventId, merchantName, eventName, eventDate, description} = eventData
+  const classUrl = 'https://walletobjects.googleapis.com/walletobjects/v1/eventTicketClass/';
+  console.log(eventDate) // todo add
+  
+  const classPayload = {
+    "id": `${GOOGLE_WALLET_ISSUER_ID}.${eventId}`,
+    "issuerName":merchantName,
+    "eventName": {
+      "defaultValue": {
+        "language": "en-US",
+        "value": eventName
+      },
+      "textModulesData": [
+      {
+        "header": eventName,
+        "body": description,
+        "id": 'description'
+      }],
+      "EventDateTime":{'start':eventDate}
+    },
+    "reviewStatus": "underReview"
+  };
+  const result = await HTTP_CLIENT.request({
+    url: classUrl,
+    method: 'POST',
+    data: classPayload
+  });
+  console.log(result)
+}
+
+async function  createGooglePassTicket(classId: string, ticketDetail: GoogleTicketDetail, id: string){
   const {ticketId, eventId, header, body, ticketHolderName} = ticketDetail
   const objectUrl = 'https://walletobjects.googleapis.com/walletobjects/v1/eventTicketObject/';
   const objectPayload = {
-    "id":ticketId,
+    id,
     eventId, 
-    "classId": `${issuerId}.${classId}`,
+    "classId":`${GOOGLE_WALLET_ISSUER_ID}.${classId}`,
     "textModulesData": [
       {
         header,
@@ -30,7 +69,7 @@ async function  createGooglePassTicket(classId: string, issuerId: string, ticket
     "state": "active",
     ticketHolderName,
   }
-  return httpClient.request({
+  await HTTP_CLIENT.request({
     url: objectUrl,
     method: 'POST',
     data: objectPayload
@@ -38,17 +77,14 @@ async function  createGooglePassTicket(classId: string, issuerId: string, ticket
 }
 
 
-export async function createGooglePassUrl(credentials, classId: string, issuerId: string, ticketDetails: Array<GoogleTicketDetail>) {
-  const {client_email, private_key} = credentials
-  const httpClient = new GoogleAuth({
-    credentials,
-    scopes: 'https://www.googleapis.com/auth/wallet_object.issuer'
-  });
+export async function createGooglePassUrl(classId: string, ticketDetails: Array<GoogleTicketDetail>) {
+  const {client_email, private_key} = CREDENTIALS
   const promises = []
-  const ticketIds = []
+  const eventTicketObjects = []
   ticketDetails.forEach((ticketDetail) => {
-    promises.push(createGooglePassTicket(classId, issuerId, ticketDetail, httpClient))
-    ticketIds.push(ticketDetail.ticketId)
+    const id = `${GOOGLE_WALLET_ISSUER_ID}.${ticketDetail.ticketId.replace(/[^\w.-]/g, '_')}-${classId}`;
+    promises.push(createGooglePassTicket(classId, ticketDetail, id))
+    eventTicketObjects.push({id})
   })
   await Promise.all(promises)
   const claims = {
@@ -57,13 +93,11 @@ export async function createGooglePassUrl(credentials, classId: string, issuerId
     origins: ['www.mercadopay.co'],
     typ: 'savetowallet',
     payload: {
-      eventTicketObjects: [{
-        id: ticketIds
-      }],
+      eventTicketObjects
     }
   };
   const token = jwt.sign(claims, private_key, { algorithm: 'RS256' });
-  console.log(token)
   return `https://pay.google.com/gp/v/save/${token}`;
 }
+
 
