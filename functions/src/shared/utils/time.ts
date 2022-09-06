ecd76ff9f5ef0bd2e9f5cd08f200aa3472c4537b
oli5679 <olicairns93@gmail.com>
2022-09-06 18:20:09 +0100

    rm unused fn

--- functions/src/shared/utils/time.ts
+++ functions/src/shared/utils/time.ts
@@ -1,5 +1,4 @@
 import { format } from "date-fns"
-import { Timestamp } from "firebase/firestore";
 
 export function dateFromTimestamp(timestamp) {
   const seconds = timestamp.seconds ?? timestamp._seconds
@@ -10,9 +9,3 @@ export function longFormat(date: Date) {
   return `${format(date, "MMM d")} at ${format(date, "HH:mm")}`
 }
 
-
-export function formatDateWithZone(timestamp: Timestamp, tz: string = 'sv') {
-  var s = timestamp.toDate().toLocaleString("en-US", {timeZone: "Europe/Belfast"});
-  var a = s.split(/\D/);
-  return a[2] + '-' + a[1] + '-' + a[0] + 'T' + a[4] + ':' + a[5] + ':' + a[6];
-}
