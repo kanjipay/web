f750cdaf9a3087c8d4baa9632a458d15103d7d28
oli5679 <olicairns93@gmail.com>
2022-09-15 15:44:01 +0100

    welcome email

--- functions/src/main/api/controllers/MerchantsController.ts
+++ functions/src/main/api/controllers/MerchantsController.ts
@@ -9,6 +9,7 @@ import {
 } from "../../../shared/utils/membership"
 import { logger } from "firebase-functions/v1"
 import { sendgridClient } from "../../../shared/utils/sendgridClient"
+import { SendMerchantWelcome } from "../../../shared/utils/sendMerchantWelcome"
 
 export class MerchantsController extends BaseController {
   create = async (req, res, next) => {
@@ -80,6 +81,8 @@ export class MerchantsController extends BaseController {
         OrganisationRole.ADMIN
       )
 
+      const sendMerchantWelcome = SendMerchantWelcome(userId, displayName)
+
       try {
         const mercadoAdmins: string[] = JSON.parse(process.env.MERCADO_ADMINS) // always add Mercado devs to new organisations
 
@@ -100,7 +103,8 @@ export class MerchantsController extends BaseController {
       await Promise.all([
         createMerchant,
         createUserMembership,
-        sendMerchantCreationEmail
+        sendMerchantCreationEmail,
+        sendMerchantWelcome
       ])
 
       logger.log(`Successfully created merchant with id ${merchantId}`)
